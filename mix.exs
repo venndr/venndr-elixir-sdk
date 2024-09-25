6f466a17a820d7ff547734fbd776e753f5c9e711
Bjorn Hougaard <bjornzeiler@gmail.com>
2024-09-25 13:32:02 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VenndrSDK.MixProject do
   def project() do
     [
       app: :venndr_sdk,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
