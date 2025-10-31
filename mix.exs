9da7c5a32e8c44d15d153d757d17f588223da46a
Niklas Lindgren <niklas@musicglue.com>
2025-10-31 14:00:20 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VenndrSDK.MixProject do
   def project() do
     [
       app: :venndr_sdk,
-      version: "0.1.4",
+      version: "0.2.0",
       elixir: "~> 1.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
